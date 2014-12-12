a66f563119c9951a9a6197ad3f89e7878bfec9ee
Aaron Hastings <aaron@aaronhastings.me>
2014-12-12 20:22:08 +0000

    Changed eval() calls to JSON.parse()

--- js/gettimes.js
+++ js/gettimes.js
@@ -19,7 +19,7 @@ WorkerScript.onMessage = function(sentMessage) {
             msg = xmlHttp.responseText;
 
             // Parse response text to usable object.
-            parsedMsg = eval("(" + msg + ")");
+            parsedMsg = JSON.parse(msg);
 
             if (typeof parsedMsg != "undefined") {
                 // Ensure that train times are available.
